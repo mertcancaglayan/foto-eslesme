6cfbef5170217cc7773f5426d7691e699f35020e
M.can <caglayan.mertcan37@gmail.com>
2023-11-11 17:14:46 +0300

    görev 1 tamamlandı

---
+++ script.js
@@ -0,0 +1,134 @@
+let puanText = document.getElementById('puanText');
+puan = 0;
+
+puanText.innerHTML = "0"
+
+const kartTemplate = `
+    <div class="kart-cerceve">
+        <div class="kart-onyuz">
+            <img src="https://via.placeholder.com/100x100?text=?">
+        </div>
+
+        <div class="kart-arkayuz">
+            <img src="">
+        </div>
+    </div>
+`;
+
+let randomNum = function(){
+    let randomArray = [];
+    for (let i = 0; i < 8; i++){
+       let randomNumbers = Math.floor(Math.random() * 99);
+        randomArray.push(randomNumbers, randomNumbers);
+        if (randomArray.length > 6) break;
+    }
+    return randomArray
+};
+
+
+/*
+Görev 2: Bu numaraları 1-99 arası(1 ve 99 dahil) sayılardan rastgele 4 çift oluşturacak şekilde üreten bir fonksiyon yazarak, kod bloğundaki array değerini bu fonksiyondan dönen array ile değiştiren kodları yazın
+*/
+const fotoNumaralari = [10, 20, 30, 20, 10, 40, 40, 30];
+
+
+console.log(fotoNumaralari);
+
+for (fotoNumara of fotoNumaralari) {
+    const yenikart = document.createElement("div");
+    yenikart.innerHTML = kartTemplate;
+    yenikart.classList.add("kart");
+    yenikart.querySelector(".kart-arkayuz img").src = `https://lipsum.app/id/${fotoNumara}/100x100`;
+    document.querySelector("div#oyun-cerceve").append(yenikart);
+
+    //Her bir karta tıklandığında "kartTiklama" fonksiyonu çalışacak.
+    yenikart.addEventListener("click", kartTiklama);
+}
+
+function kartTiklama(olay) {
+    //Tıklanan kartı seçilen olarak değişkene atayalım
+    const secilenKart = olay.currentTarget;
+
+    //Tıklanan kart eslesti classına sahipse daha önce başka kartla eşleşmiş ve zaten açık durumda demektir, işlem yapmayacağız.
+    if (secilenKart.classList.contains("eslesti") === true) {
+        return;
+    }
+
+    //Tıklanan ve açılan karta tekrar tıklanırsa işlem yapmayacağız.
+    if (secilenKart.classList.contains("acik") === true) {
+        return;
+    }
+
+    //Peşpeşe kartlara tıklandığında 2'den fazla kart tıklanmasını engellememiz gerekiyor.
+    const tumAcikKartlar = document.querySelectorAll(".acik");
+    if (tumAcikKartlar.length === 2) {
+        return;
+    }
+
+    //Açık olan kart varsa seçelim.
+    const acikKart = document.querySelector(".acik");
+
+    //Hiç açık kart yoksa tıklanan karta açık class veriyoruz ve fonksiyondan çıkıyoruz.
+    if (acikKart === null) {
+        secilenKart.classList.add("acik");
+
+        setTimeout(
+            () => {
+                secilenKart.classList.remove("acik");
+            }, 1500
+        );
+        return;
+    }
+
+    //Daha önce bir açık kartımız varmış, son seçilen karta da açık class vererek tersini çevirelim.
+    secilenKart.classList.add("acik");
+
+    //Açık kartlara ait img etiketlerinin src görsel dosyaları eşleşiyor mu?
+    const acikKartImg = acikKart.querySelector(".kart-arkayuz img");
+    const secilenKartImg = secilenKart.querySelector(".kart-arkayuz img");
+
+    if (acikKartImg.src === secilenKartImg.src) {
+      //İki açık kart arasında eşleşme var.
+      acikKart.classList.add("eslesti");
+      secilenKart.classList.add("eslesti");
+
+      puan++;
+
+      console.log(puan);
+
+      puanText.innerHTML = puan;
+        // görev 1, html e embeded code ekledim sonrasında display none verdim, oyun bittiğinde tekrardan display edip sayfanın ortasına aldım
+      if (puan === 4) {
+        const kutlamaGorseli = document.querySelector("#kutlamaGorseli");
+        kutlamaGorseli.style.position = "absolute";
+        kutlamaGorseli.style.top = "0";
+        kutlamaGorseli.style.left = "0";
+        kutlamaGorseli.style.top = "50%";
+        kutlamaGorseli.style.left = "50%";
+        kutlamaGorseli.style.transform = "translate(-50%, -50%)";
+        kutlamaGorseli.style.display = "flex"
+       
+
+
+        setTimeout(() => {
+            kutlamaGorseli.remove();
+        }, 5000);
+    }
+
+
+      acikKart.classList.remove("acik");
+      secilenKart.classList.remove("acik");
+
+      setTimeout(() => {
+        acikKart.removeEventListener("click", kartTiklama);
+        secilenKart.removeEventListener("click", kartTiklama);
+      }, 1000);
+    } else {
+        //İki açık kartın görsel dosya adı birbirinden farklı, eşleşme yok, kartlar kapansın.
+        setTimeout(() => {
+            acikKart.classList.remove("acik");
+            secilenKart.classList.remove("acik");
+        }, 1500);
+    }
+}
+
